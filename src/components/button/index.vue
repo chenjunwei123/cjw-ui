7a616b5585eb43dc062529426d8bf5792a5a1303
cjw <15316117075@163.com>
2023-07-18 21:59:47 +0800

    项目初始化

---
+++ src/components/button/index.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-07-18 21:34:25
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-07-18 21:53:39
+ * @FilePath: \前端\cjw-ui\src\components\button\index.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEvc
+-->
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        key: value
+      }
+    },
+    methods: {
+      name() {
+
+      }
+    },
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
